926f74bf6b2bfb01a3765fff7012bc81f55cc81a
Anmol Chawda <anmolchawda@gmail.com>
2025-05-24 18:03:39 +0000

    Make the following changes:
    ,
    
    Find this element:
    
      <X className="h-4 w-4">
    
    Relevant files:
    
        - https://6000-firebase-studio-1748099875903.cluster-htdgsbmflbdmov5xrjithceibm.cloudworkstations.dev/_next/static/chunks/node_modules_4c9772c7._.js
        - https://6000-firebase-studio-1748099875903.cluster-htdgsbmflbdmov5xrjithceibm.cloudworkstations.dev/_next/static/chunks/src_a426fad4._.js
        - https://6000-firebase-studio-1748099875903.cluster-htdgsbmflbdmov5xrjithceibm.cloudworkstations.dev/_next/static/chunks/node_modules_4c9772c7._.js
        - https://6000-firebase-studio-1748099875903.cluster-htdgsbmflbdmov5xrjithceibm.cloudworkstations.dev/_next/static/chunks/node_modules_next_dist_compiled_2ce9398a._.js
    
    And change it as follows:
    
    remove this button

--- src/components/post/share-modal.tsx
+++ src/components/post/share-modal.tsx
@@ -73,10 +73,7 @@ export function ShareModal({ isOpen, onOpenChange, postUrl, postCaption }: Share
             <LinkIcon className="mr-3 h-6 w-6" /> Copy Link
           </Button>
         </div>
-         <Button variant="ghost" onClick={() => onOpenChange(false)} className="absolute right-4 top-4 rounded-sm opacity-70 ring-offset-background transition-opacity hover:opacity-100 focus:outline-none focus:ring-2 focus:ring-ring focus:ring-offset-2 disabled:pointer-events-none data-[state=open]:bg-accent data-[state=open]:text-muted-foreground">
-            <X className="h-4 w-4" />
-            <span className="sr-only">Close</span>
-        </Button>
+        {/* The X button was here, now removed */}
       </DialogContent>
     </Dialog>
   );
