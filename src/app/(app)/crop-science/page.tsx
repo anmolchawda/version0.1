9c6bedcd4e6c5e3def8bd9b3a9360a1da73c538c
Anmol Chawda <anmolchawda@gmail.com>
2025-05-26 13:12:24 +0000

    Make the following changes:
    ,
    
    Find this element:
    
    Relevant files:
    
        - https://6000-firebase-studio-1748099875903.cluster-htdgsbmflbdmov5xrjithceibm.cloudworkstations.dev/_next/static/chunks/node_modules_next_dist_compiled_2ce9398a._.js
        - https://6000-firebase-studio-1748099875903.cluster-htdgsbmflbdmov5xrjithceibm.cloudworkstations.dev/_next/static/chunks/node_modules_next_dist_compiled_2ce9398a._.js
    
    And change it as follows:
    
    Update the following images in this component:
    
    Image 1:
             - Original image: /_next/image?url=https%3A%2F%2Fimages.unsplash.com%2Fphoto-1580157508103-2a4e9fe8ed29%3Fcrop%3Dentropy%26cs%3Dtinysrgb%26fit%3Dmax%26fm%3Djpg%26ixid%3DM3w3NDE5ODJ8MHwxfHNlYXJjaHw0fHxwb21lZ3JhbmF0ZSUyMGZydWl0fGVufDB8fHx8MTc0ODIzODcyOXww%26ixlib%3Drb-4.1.0%26q%3D80%26w%3D1080&w=384&q=75
             - New image URL: https://images.unsplash.com/photo-1615485925600-97237c4fc1ec?crop=entropy&cs=tinysrgb&fit=max&fm=jpg&ixid=M3w3NDE5ODJ8MHwxfHNlYXJjaHw2fHxwb21lZ3JhbmF0ZXxlbnwwfHx8fDE3NDgyNDc4Mjl8MA&ixlib=rb-4.1.0&q=80&w=1080
             - Hint used: "pomegranate fruit"
    
    Replace the image sources with the new URLs. This may involve updating src, srcset, background-image, or other relevant attributes depending on the element type.

--- src/app/(app)/crop-science/page.tsx
+++ src/app/(app)/crop-science/page.tsx
@@ -33,7 +33,7 @@ const commonCrops: CropInfo[] = [
   { name: 'Strawberry', imageUrl: 'https://images.unsplash.com/photo-1568966299181-bb7282cc84f0?crop=entropy&cs=tinysrgb&fit=max&fm=jpg&ixid=M3w3NDE5ODJ8MHwxfHNlYXJjaHw4fHxzdHJhd2JlcnJ5fGVufDB8fHx8MTc0ODI0NDc0Mnww&ixlib=rb-4.1.0&q=80&w=1080', aiHint: 'strawberry fruit' },
   { name: 'Pineapple', imageUrl: 'https://images.unsplash.com/photo-1490885578174-acda8905c2c6?crop=entropy&cs=tinysrgb&fit=max&fm=jpg&ixid=M3w3NDE5ODJ8MHwxfHNlYXJjaHwzfHxwaW5lYXBwbGV8ZW58MHx8fHwxNzQ4MjQ3NjcwfDA&ixlib=rb-4.1.0&q=80&w=1080', aiHint: 'pineapple fruit' },
   { name: 'Watermelon', imageUrl: 'https://images.unsplash.com/photo-1587049352846-4a222e784d38?crop=entropy&cs=tinysrgb&fit=max&fm=jpg&ixid=M3w3NDE5ODJ8MHwxfHNlYXJjaHw2fHx3YXRlcm1lbG9ufGVufDB8fHx8MTc0ODI0NzY1Nnww&ixlib=rb-4.1.0&q=80&w=1080', aiHint: 'watermelon fruit' },
-  { name: 'Pomegranate', imageUrl: 'https://images.unsplash.com/photo-1580157508103-2a4e9fe8ed29?crop=entropy&cs=tinysrgb&fit=max&fm=jpg&ixid=M3w3NDE5ODJ8MHwxfHNlYXJjaHw0fHxwb21lZ3JhbmF0ZSUyMGZydWl0fGVufDB8fHx8MTc0ODIzODcyOXww&ixlib=rb-4.1.0&q=80&w=1080', aiHint: 'pomegranate fruit' },
+  { name: 'Pomegranate', imageUrl: 'https://images.unsplash.com/photo-1615485925600-97237c4fc1ec?crop=entropy&cs=tinysrgb&fit=max&fm=jpg&ixid=M3w3NDE5ODJ8MHwxfHNlYXJjaHw2fHxwb21lZ3JhbmF0ZXxlbnwwfHx8fDE3NDgyNDc4Mjl8MA&ixlib=rb-4.1.0&q=80&w=1080', aiHint: 'pomegranate fruit' },
   { name: 'Kiwi', imageUrl: 'https://images.unsplash.com/photo-1572539280469-9c738c59964d?crop=entropy&cs=tinysrgb&fit=max&fm=jpg&ixid=M3w3NDE5ODJ8MHwxfHNlYXJjaHwxMHx8a2l3aSUyMGZydWl0fGVufDB8fHx8MTc0ODIzODcyOXww&ixlib=rb-4.1.0&q=80&w=1080', aiHint: 'kiwi fruit' },
 ];
 
