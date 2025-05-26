3562395f367a9483b4a82adda7bffe8db9859a5f
Anmol Chawda <anmolchawda@gmail.com>
2025-05-26 17:10:58 +0000

    Make the following changes:
    ,
    
    Find this element:
    
    Relevant files:
    
        - https://6000-firebase-studio-1748099875903.cluster-htdgsbmflbdmov5xrjithceibm.cloudworkstations.dev/_next/static/chunks/node_modules_next_dist_compiled_2ce9398a._.js
        - https://6000-firebase-studio-1748099875903.cluster-htdgsbmflbdmov5xrjithceibm.cloudworkstations.dev/_next/static/chunks/node_modules_next_dist_compiled_2ce9398a._.js
    
    And change it as follows:
    
    https://storage.cloud.google.com/imagesoffarmdocc/crop%20images/orange.jpg use this URL

--- src/app/(app)/crop-science/page.tsx
+++ src/app/(app)/crop-science/page.tsx
@@ -39,7 +39,7 @@ const commonCrops: CropInfo[] = [
   // Fruits
   { name: 'Apple', imageUrl: 'https://storage.cloud.google.com/imagesoffarmdocc/crop%20images/apple.jpg', aiHint: 'apple fruit', slug: 'apple', unoptimized: true },
   { name: 'Banana', imageUrl: 'https://storage.cloud.google.com/imagesoffarmdocc/crop%20images/banana.jpg', aiHint: 'banana fruit', slug: 'banana', unoptimized: true },
-  { name: 'Orange', imageUrl: 'https://images.unsplash.com/photo-1609424572698-04d9d2e04954?crop=entropy&cs=tinysrgb&fit=max&fm=jpg&ixid=M3w3NDE5ODJ8MHwxfHNlYXJjaHwxNnx8b3JhbmdlfGVufDB8fHx8MTc0ODI0NDY5OHww&ixlib=rb-4.1.0&q=80&w=1080', aiHint: 'orange fruit', slug: 'orange' },
+  { name: 'Orange', imageUrl: 'https://storage.cloud.google.com/imagesoffarmdocc/crop%20images/orange.jpg', aiHint: 'orange fruit', slug: 'orange', unoptimized: true },
   { name: 'Mango', imageUrl: 'https://images.unsplash.com/photo-1553279768-865429fa0078?crop=entropy&cs=tinysrgb&fit=max&fm=jpg&ixid=M3w3NDE5ODJ8MHwxfHNlYXJjaHwxfHxtYW5nb3xlbnwwfHx8fDE3NDgyNDc2ODJ8MA&ixlib=rb-4.1.0&q=80&w=1080', aiHint: 'mango fruit', slug: 'mango' },
   { name: 'Grapes', imageUrl: 'https://images.unsplash.com/photo-1596363505729-4190a9506133?crop=entropy&cs=tinysrgb&fit=max&fm=jpg&ixid=M3w3NDE5ODJ8MHwxfHNlYXJjaHwxfHxncmFwZXN8ZW58MHx8fHwxNzQ4MjQ0NzE2fDA&ixlib=rb-4.1.0&q=80&w=1080', aiHint: 'grapes fruit', slug: 'grapes' },
   { name: 'Strawberry', imageUrl: 'https://images.unsplash.com/photo-1568966299181-bb7282cc84f0?crop=entropy&cs=tinysrgb&fit=max&fm=jpg&ixid=M3w3NDE5ODJ8MHwxfHNlYXJjaHw4fHxzdHJhd2JlcnJ5fGVufDB8fHx8MTc0ODI0NDc0Mnww&ixlib=rb-4.1.0&q=80&w=1080', aiHint: 'strawberry fruit', slug: 'strawberry' },
