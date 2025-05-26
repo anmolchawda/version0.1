7f094fc30d5e06097c2ad235fe271291686d15a2
Anmol Chawda <anmolchawda@gmail.com>
2025-05-26 18:31:34 +0000

    Make the following changes:
    ,
    
    Find this element:
    
    Relevant files:
    
        - https://6000-firebase-studio-1748099875903.cluster-htdgsbmflbdmov5xrjithceibm.cloudworkstations.dev/_next/static/chunks/node_modules_4c9772c7._.js
        - https://6000-firebase-studio-1748099875903.cluster-htdgsbmflbdmov5xrjithceibm.cloudworkstations.dev/_next/static/chunks/node_modules_4c9772c7._.js
        - https://6000-firebase-studio-1748099875903.cluster-htdgsbmflbdmov5xrjithceibm.cloudworkstations.dev/_next/static/chunks/node_modules_4c9772c7._.js
        - https://6000-firebase-studio-1748099875903.cluster-htdgsbmflbdmov5xrjithceibm.cloudworkstations.dev/_next/static/chunks/node_modules_next_dist_compiled_2ce9398a._.js
        - https://6000-firebase-studio-1748099875903.cluster-htdgsbmflbdmov5xrjithceibm.cloudworkstations.dev/_next/static/chunks/node_modules_next_dist_compiled_2ce9398a._.js
    
    And change it as follows:
    
    https://storage.cloud.google.com/imagesoffarmdocc/KrishiX/KrishiX%20PNG%20Logo.png use this URL

--- src/components/core/app-logo.tsx
+++ src/components/core/app-logo.tsx
@@ -6,15 +6,9 @@ import { cn } from '@/lib/utils';
 export function AppLogo({ className, textClassName, iconClassName }: { className?: string; textClassName?: string; iconClassName?: string }) {
   return (
     <Link href="/" className={cn("flex items-center gap-2 text-2xl font-bold text-primary", className)}>
-      {/* 
-        Please place your logo image (e.g., logo.png, logo.svg) 
-        in the 'public' folder at the root of your project.
-        If your image is named differently or has a different extension, 
-        update the 'src' attribute below accordingly.
-      */}
       <div className={cn("relative h-8 w-8", iconClassName)}> {/* Container for the image */}
         <Image
-          src="/logo.png" // Assumes your logo is named logo.png and is in the /public folder
+          src="https://storage.cloud.google.com/imagesoffarmdocc/KrishiX/KrishiX%20PNG%20Logo.png"
           alt="KrishiX Logo"
           layout="fill" // Fills the container div
           objectFit="contain" // Ensures the whole logo is visible and maintains aspect ratio
