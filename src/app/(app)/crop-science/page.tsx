2f57e863b81c90aa054b1b1a249c643e1feb399d
Anmol Chawda <anmolchawda@gmail.com>
2025-05-26 16:38:00 +0000

    Make the following changes:
    ,
    
    Find this element:
    
    Relevant files:
    
        - https://6000-firebase-studio-1748099875903.cluster-htdgsbmflbdmov5xrjithceibm.cloudworkstations.dev/_next/static/chunks/node_modules_next_dist_compiled_2ce9398a._.js
        - https://6000-firebase-studio-1748099875903.cluster-htdgsbmflbdmov5xrjithceibm.cloudworkstations.dev/_next/static/chunks/node_modules_next_dist_compiled_2ce9398a._.js
    
    And change it as follows:
    
    https://storage.cloud.google.com/imagesoffarmdocc/crop%20images/cauliflower.jpeg use this URL

--- src/app/(app)/crop-science/page.tsx
+++ src/app/(app)/crop-science/page.tsx
@@ -31,7 +31,7 @@ const commonCrops: CropInfo[] = [
   { name: 'Bottle Gourd', imageUrl: 'https://storage.cloud.google.com/imagesoffarmdocc/crop%20images/bottle%20gourd.jpeg', aiHint: 'bottle gourd', slug: 'bottle-gourd', unoptimized: true },
   { name: 'Bitter Gourd', imageUrl: 'https://storage.cloud.google.com/imagesoffarmdocc/crop%20images/bitter%20gourd.jpeg', aiHint: 'bitter gourd', slug: 'bitter-gourd', unoptimized: true },
   { name: 'Cabbage', imageUrl: 'https://storage.cloud.google.com/imagesoffarmdocc/crop%20images/cabbage.jpeg', aiHint: 'cabbage vegetable', slug: 'cabbage', unoptimized: true },
-  { name: 'Cauliflower', imageUrl: 'https://placehold.co/200x200.png?text=Cauliflower', aiHint: 'cauliflower vegetable', slug: 'cauliflower' },
+  { name: 'Cauliflower', imageUrl: 'https://storage.cloud.google.com/imagesoffarmdocc/crop%20images/cauliflower.jpeg', aiHint: 'cauliflower vegetable', slug: 'cauliflower', unoptimized: true },
   { name: 'Radish', imageUrl: 'https://placehold.co/200x200.png?text=Radish', aiHint: 'radish vegetable', slug: 'radish' },
   { name: 'Ridge Gourd', imageUrl: 'https://placehold.co/200x200.png?text=Ridge+Gourd', aiHint: 'ridge gourd', slug: 'ridge-gourd' },
   { name: 'Drumstick', imageUrl: 'https://placehold.co/200x200.png?text=Drumstick', aiHint: 'drumstick vegetable', slug: 'drumstick' },
@@ -99,6 +99,7 @@ export default function CropSciencePage() {
     
 
     
+
 
 
 
