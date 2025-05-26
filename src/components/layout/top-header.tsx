b5d656b23be7b2945edb1631f5af7c23a57aef5b
Anmol Chawda <anmolchawda@gmail.com>
2025-05-26 19:03:05 +0000

    why krishix logo image is not showing in mobile preview?

--- src/components/layout/top-header.tsx
+++ src/components/layout/top-header.tsx
@@ -25,7 +25,7 @@ export function TopHeader() {
 
       {/* Center: App Logo */}
       <div className="absolute left-1/2 top-1/2 -translate-x-1/2 -translate-y-1/2">
-        <AppLogo iconClassName="h-20 w-20" textClassName="hidden" />
+        <AppLogo iconClassName="h-12 w-12" textClassName="hidden" />
       </div>
 
       {/* Right: Messages Icon */}
