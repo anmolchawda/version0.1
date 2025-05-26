8ce071bf79d8f54a537687db650fb608aa1fb896
Anmol Chawda <anmolchawda@gmail.com>
2025-05-26 16:23:45 +0000

    Make the following changes:
    ,
    
    Find this element:
    
    Relevant files:
    
        - https://6000-firebase-studio-1748099875903.cluster-htdgsbmflbdmov5xrjithceibm.cloudworkstations.dev/_next/static/chunks/node_modules_next_dist_compiled_2ce9398a._.js
        - https://6000-firebase-studio-1748099875903.cluster-htdgsbmflbdmov5xrjithceibm.cloudworkstations.dev/_next/static/chunks/node_modules_next_dist_compiled_2ce9398a._.js
    
    And change it as follows:
    
    https://storage.cloud.google.com/imagesoffarmdocc/crop%20images/bottle%20gourd.jpeg use this URL

--- src/app/(app)/crop-science/page.tsx
+++ src/app/(app)/crop-science/page.tsx
@@ -28,7 +28,7 @@ const commonCrops: CropInfo[] = [
   { name: 'Lettuce', imageUrl: 'https://images.unsplash.com/photo-1622943495354-f49d2964094c?crop=entropy&cs=tinysrgb&fit=max&fm=jpg&ixid=M3w3NDE5ODJ8MHwxfHNlYXJjaHwxNHx8bGV0dHVjZXxlbnwwfHx8fDE3NDgyMzk5Mjh8MA&ixlib=rb-4.1.0&q=80&w=1080', aiHint: 'lettuce leaves', slug: 'lettuce' },
   { name: 'Eggplant', imageUrl: 'https://images.unsplash.com/photo-1615484477201-9f4953340fab?crop=entropy&cs=tinysrgb&fit=max&fm=jpg&ixid=M3w3NDE5ODJ8MHwxfHNlYXJjaHwxfHxlZ2dwbGFudHxlbnwwfHx8fDE3NDgyNDAwNDZ8MA&ixlib=rb-4.1.0&q=80&w=1080', aiHint: 'eggplant vegetable', slug: 'eggplant' },
   { name: 'Beans', imageUrl: 'https://storage.cloud.google.com/imagesoffarmdocc/crop%20images/Beans.jpeg', aiHint: 'beans vegetable', slug: 'beans', unoptimized: true },
-  { name: 'Bottle Gourd', imageUrl: 'https://placehold.co/200x200.png?text=Bottle+Gourd', aiHint: 'bottle gourd', slug: 'bottle-gourd' },
+  { name: 'Bottle Gourd', imageUrl: 'https://storage.cloud.google.com/imagesoffarmdocc/crop%20images/bottle%20gourd.jpeg', aiHint: 'bottle gourd', slug: 'bottle-gourd', unoptimized: true },
   { name: 'Bitter Gourd', imageUrl: 'https://placehold.co/200x200.png?text=Bitter+Gourd', aiHint: 'bitter gourd', slug: 'bitter-gourd' },
   { name: 'Cabbage', imageUrl: 'https://placehold.co/200x200.png?text=Cabbage', aiHint: 'cabbage vegetable', slug: 'cabbage' },
   { name: 'Cauliflower', imageUrl: 'https://placehold.co/200x200.png?text=Cauliflower', aiHint: 'cauliflower vegetable', slug: 'cauliflower' },
@@ -99,4 +99,5 @@ export default function CropSciencePage() {
     
 
     
+
 
