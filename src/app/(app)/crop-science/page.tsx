003e9ead0e1a8e304f6ecbc5da1524844206d27d
Anmol Chawda <anmolchawda@gmail.com>
2025-05-29 02:24:50 +0000

    Make the following changes:
    ,
    
    Find this element:
    
    Relevant files:
    
        - https://6000-firebase-studio-1748099875903.cluster-htdgsbmflbdmov5xrjithceibm.cloudworkstations.dev/_next/static/chunks/node_modules_next_dist_compiled_2ce9398a._.js
        - https://6000-firebase-studio-1748099875903.cluster-htdgsbmflbdmov5xrjithceibm.cloudworkstations.dev/_next/static/chunks/node_modules_next_dist_compiled_2ce9398a._.js
    
    And change it as follows:
    
    https://firebasestorage.googleapis.com/v0/b/fieldverse-m99ip.firebasestorage.app/o/Crop%20images%2Fpotato.jpg?alt=media&token=f973faf2-6976-4d9a-9e2e-3c4c7e2bbc3b

--- src/app/(app)/crop-science/page.tsx
+++ src/app/(app)/crop-science/page.tsx
@@ -19,7 +19,7 @@ interface CropInfo {
 const commonCrops: CropInfo[] = [
   // Vegetables
   { name: 'Tomato', imageUrl: 'https://storage.cloud.google.com/imagesoffarmdocc/crop%20images/tomato%201.jpeg', aiHint: 'tomato white-background', slug: 'tomato', unoptimized: true },
-  { name: 'Potato', imageUrl: 'https://storage.cloud.google.com/imagesoffarmdocc/crop%20images/potato.jpg', aiHint: 'potato vegetable', slug: 'potato', unoptimized: true },
+  { name: 'Potato', imageUrl: 'https://firebasestorage.googleapis.com/v0/b/fieldverse-m99ip.firebasestorage.app/o/Crop%20images%2Fpotato.jpg?alt=media&token=f973faf2-6976-4d9a-9e2e-3c4c7e2bbc3b', aiHint: 'potato vegetable', slug: 'potato', unoptimized: true },
   { name: 'Onion', imageUrl: 'https://firebasestorage.googleapis.com/v0/b/fieldverse-m99ip.firebasestorage.app/o/Crop%20images%2FONION.jpeg?alt=media&token=110b1e81-e150-4353-ad3f-ccdb45efb616', aiHint: 'onion vegetable', slug: 'onion', unoptimized: true },
   { name: 'Carrot', imageUrl: 'https://firebasestorage.googleapis.com/v0/b/fieldverse-m99ip.firebasestorage.app/o/Crop%20images%2Fcarrot.jpg?alt=media&token=570b5735-ea6b-45fb-91db-5fa6c95ec198', aiHint: 'carrot vegetable', slug: 'carrot', unoptimized: true },
   { name: 'Broccoli', imageUrl: 'https://firebasestorage.googleapis.com/v0/b/fieldverse-m99ip.firebasestorage.app/o/Crop%20images%2FBROCCOLI.jpg?alt=media&token=1ea3e09d-cbbc-44be-885f-d7a1a60c7281', aiHint: 'broccoli vegetable', slug: 'broccoli', unoptimized: true },
@@ -132,3 +132,4 @@ export default function CropSciencePage() {
 // Updated image for Orange with new Firebase Storage URL
 // Updated image for Pineapple with new Firebase Storage URL
 // Updated Pomegranate image URL
+// Updated Potato image URL with Firebase Storage link
