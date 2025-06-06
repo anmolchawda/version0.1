e1d3062d637e0b11b6ec06d956c8a614fc4ab26c
Anmol Chawda <anmolchawda@gmail.com>
2025-06-06 14:23:49 +0000

    Make the following changes:
    ,
    
    Find this element:
    
      <div style="font-f

--- src/app/(app)/profile/edit/page.tsx
+++ src/app/(app)/profile/edit/page.tsx
@@ -1,7 +1,19 @@
-// This file is effectively being replaced by settings/page.tsx
-// To ensure no build errors if a file is expected, this placeholder content can be used.
-// The user's intent is to have /settings be the new page.
+// src/app/(app)/profile/edit/page.tsx
+'use client'; // notFound can be used in client components if called during render
 
-export default function OldEditProfilePagePlaceholder() {
-  return null;
+import { notFound } from 'next/navigation';
+import { useEffect } from 'react';
+
+// This page is deprecated and should no longer be used.
+// It has been replaced by /settings/account.
+// Accessing this route will now explicitly trigger a 404.
+
+export default function OldEditProfilePage() {
+  useEffect(() => {
+    notFound();
+  }, []);
+
+  // This return will likely not be reached due to notFound()
+  // but is kept for structural completeness if notFound() behavior changes.
+  return null; 
 }
