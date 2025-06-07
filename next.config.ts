a066970faf37ce867867b26327c3047efcda24d9
Anmol Chawda <anmolchawda@gmail.com>
2025-06-07 08:18:42 +0000

    I see this error with the app, reported by NextJS, please fix it. The er
    Former-commit-id: 1e21f66e493538dbfba96e91c72cde74270d121d

--- next.config.ts
+++ next.config.ts
@@ -1,6 +1,22 @@
 import type { NextConfig } from 'next';
 
 const nextConfig: NextConfig = {
+  images: {
+    remotePatterns: [
+      {
+        protocol: 'https',
+        hostname: 'images.unsplash.com',
+      },
+      {
+        protocol: 'https',
+        hostname: 'placehold.co',
+      },
+      {
+        protocol: 'https',
+        hostname: 'firebasestorage.googleapis.com',
+      },
+    ],
+  },
   // The experimental.allowedDevOrigins option has been removed
   // as it was deprecated in Next.js 14 and is not supported in Next.js 15.
   // Default CORS handling for development should suffice for most cases.
