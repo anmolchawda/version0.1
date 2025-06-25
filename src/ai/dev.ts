4d4f895736e78b426c0afe717a8e122d17936c83
Anmol Chawda <anmolchawda@gmail.com>
2025-06-25 15:07:05 +0000

    The NextJS app server is having trouble starting. Please identify what c

--- src/ai/dev.ts
+++
@@ -1,5 +0,0 @@
-import { config } from 'dotenv';
-config();
-
-import './flows/smart-hashtag-suggestions.ts';
-import './flows/analyze-crop-image-flow.ts';
--- src/app/(app)/post/create/page.tsx
+++
@@ -1,9 +0,0 @@
-import { CreatePostForm } from '@/components/post/create-post-form';
-
-export default function CreatePostPage() {
-  return (
-    <div className="py-8">
-      <CreatePostForm />
-    </div>
-  );
-}
