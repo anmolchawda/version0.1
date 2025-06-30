39ea349b80507830f67a1c1d17e99d6ca65e4931
Anmol Chawda <anmolchawda@gmail.com>
2025-06-30 10:14:18 +0000

    I see this error with the app, reported by NextJS, please fix it. The er

--- next.config.ts
+++ next.config.ts
@@ -1,6 +1,5 @@
-import type { NextConfig } from 'next';
-
-const nextConfig: NextConfig = {
+/** @type {import('next').NextConfig} */
+const nextConfig = {
   images: {
     remotePatterns: [
       {
@@ -15,6 +14,11 @@ const nextConfig: NextConfig = {
         protocol: 'https',
         hostname: 'firebasestorage.googleapis.com',
       },
+    ],
+  },
+  experimental: {
+    allowedDevOrigins: [
+      'https://6000-firebase-studio-1748099875903.cluster-htdgsbmflbdmov5xrjithceibm.cloudworkstations.dev',
     ],
   },
 };
--- src/next.config.ts
+++ src/next.config.ts
@@ -19,7 +19,6 @@ const nextConfig: NextConfig = {
   },
   experimental: {
     // This is required to allow the Next.js dev server to be accessed from the Firebase Studio preview pane.
-    allowedDevOrigins: ['https://*.cloudworkstations.dev'],
   },
 };
 
