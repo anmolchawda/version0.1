4b72f2daa212ed55251655d743f3afb1c321d58d
Anmol Chawda <anmolchawda@gmail.com>
2026-02-28 17:03:21 +0000

    Try fixing this error: `Runtime Error: Invalid src prop (https://firebas

--- next.config.ts
+++ next.config.ts
@@ -18,6 +18,7 @@ const nextConfig: NextConfig = {
     ],
   },
   experimental: {
+    serverActions: true,
     // This is required to allow the Next.js dev server to be accessed from the Firebase Studio preview pane.
   },
 };
