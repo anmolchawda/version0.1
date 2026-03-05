b1f6e9bde7fd423f4c4bb7730d90b6e893b12156
Anmol Chawda <anmolchawda@gmail.com>
2026-03-05 19:53:58 +0000

    Try fixing this error: `Runtime Error: Invalid src prop (https://firebas

--- next.config.js
+++ next.config.js
@@ -1,6 +1,5 @@
-import type { NextConfig } from 'next';
-
-const nextConfig: NextConfig = {
+/** @type {import('next').NextConfig} */
+const nextConfig = {
   images: {
     remotePatterns: [
       {
@@ -19,8 +18,7 @@ const nextConfig: NextConfig = {
   },
   experimental: {
     serverActions: true,
-    // This is required to allow the Next.js dev server to be accessed from the Firebase Studio preview pane.
   },
 };
 
-export default nextConfig;
+module.exports = nextConfig;
